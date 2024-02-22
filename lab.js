a627b11f39f0d32457f750c970cadda5770ecd6c
Prince <akice@AkicePC.pc.net>
2024-02-22 09:41:13 -0600

    Feat: Text bar and send button designed

--- lab.js
+++ lab.js
@@ -1,50 +1,160 @@
-document.body.style.margin = 0
+document.body.style.margin = 0;
 
 //Paleta de colores
-let background = "black"
-let border = "#9F2042"
-let messages = "9BC995"
-let chats = "646E68"
-let words = "F8E5EE"
+let background = "black";
+let border = "#9F2042";
+let messages = "#9BC995";
+let chats = "#267891";
+let words = "#F8E5EE";
 
-let div1 = document.createElement("div")
-div1.id = "contenedor"
-document.body.appendChild(div1)
+function actualizarColores() {
+    document.getElementById("listado-chats").style.border = "1px solid " + border;
+    document.getElementById("listado-chats").style.backgroundColor = background;
 
-document.getElementById("contenedor").style.display = "grid"
-document.getElementById("contenedor").style.gridTemplateColumns = "20% 80%"
-document.getElementById("contenedor").style.gridTemplateRows = "94% 6%"
-document.getElementById("contenedor").style.border = "1px solid #9F2042"
-document.getElementById("contenedor").style.height = "calc(100vh - 0px)"
+    document.getElementById("mensaje").style.border = "1px solid " + border;
+    document.getElementById("mensaje").style.backgroundColor = background;
 
-let div2 = document.createElement("div")
-div2.id = "listado-chats"
-div1.appendChild(div2)
+    document.getElementById("contenido-perfil").style.border = "1px solid " + border;
+    document.getElementById("contenido-perfil").style.backgroundColor = background;
 
-document.getElementById("listado-chats").style.border = "1px solid #9F2042"
-document.getElementById("listado-chats").style.backgroundColor = background
+    document.getElementById("contenido-chat").style.border = "1px solid " + border;
+    document.getElementById("contenido-chat").style.backgroundColor = background;
 
-let div3 = document.createElement("div")
-div3.id = "mensaje"
-div1.appendChild(div3)
+    document.getElementById("nombre").style.color = words;
 
-document.getElementById("mensaje").style.border = "1px solid #9F2042"
-document.getElementById("mensaje").style.backgroundColor = background
+}
 
-let div4 = document.createElement("div")
-div4.id = "contenido-perfil"
-div1.appendChild(div4)
+let div1 = document.createElement("div");
+div1.id = "contenedor";
+document.body.appendChild(div1);
 
-document.getElementById("contenido-perfil").style.border = "1px solid #9F2042"
-document.getElementById("contenido-perfil").style.backgroundColor = background
+document.getElementById("contenedor").style.display = "grid";
+document.getElementById("contenedor").style.gridTemplateColumns = "20% 80%";
+document.getElementById("contenedor").style.gridTemplateRows = "94% 6%";
+document.getElementById("contenedor").style.border = "1px solid border";
+document.getElementById("contenedor").style.height = "100vh";
 
-let div5 = document.createElement("div")
-div5.id = "contenido-chat"
-div1.appendChild(div5)
+let div2 = document.createElement("div");
+div2.id = "listado-chats";
+div1.appendChild(div2);
 
-document.getElementById("contenido-chat").style.border = "1px solid #9F2042"
-document.getElementById("contenido-chat").style.backgroundColor = background
+document.getElementById("listado-chats").style.border = "1px solid " + border;
+document.getElementById("listado-chats").style.backgroundColor = background;
 
+let div3 = document.createElement("div");
+div3.id = "mensaje";
+div1.appendChild(div3);
 
+document.getElementById("mensaje").style.border = "1px solid " + border;
+document.getElementById("mensaje").style.backgroundColor = background;
 
+let div4 = document.createElement("div");
+div4.id = "contenido-perfil";
+div1.appendChild(div4);
 
+document.getElementById("contenido-perfil").style.border = "1px solid " + border;
+document.getElementById("contenido-perfil").style.backgroundColor = background;
+document.getElementById("contenido-perfil").style.display = "flex";
+document.getElementById("contenido-perfil").style.alignItems = "center";
+document.getElementById("contenido-perfil").style.justifyContent = "space-around";
+
+let div5 = document.createElement("div");
+div5.id = "contenido-chat";
+div1.appendChild(div5);
+
+document.getElementById("contenido-chat").style.border = "1px solid " + border;
+document.getElementById("contenido-chat").style.backgroundColor = background;
+document.getElementById("contenido-chat").style.display = "flex";
+document.getElementById("contenido-chat").style.alignItems = "center";
+document.getElementById("contenido-chat").style.justifyContent = "space-evenly";
+
+//Image de perfil
+let image = document.createElement("img");
+image.id = "perfil";
+div4.appendChild(image);
+
+document.getElementById("perfil").src = "https://media.posterlounge.com/img/products/760000/757344/757344_poster.jpg";
+document.getElementById("perfil").style.width = "50px";
+document.getElementById("perfil").style.height = "50px";
+document.getElementById("perfil").style.borderRadius = "50%";
+
+let nombre_perfil = document.createElement("p");
+nombre_perfil.id = "nombre";
+div4.appendChild(nombre_perfil);
+
+document.getElementById("nombre").textContent = "José Prince";
+document.getElementById("nombre").style.color = words;
+
+let color_switch = document.createElement("label");
+color_switch.classList.add("switch");
+
+let switchInput = document.createElement("input");
+switchInput.type = "checkbox";
+switchInput.id = "toggleSwitch";
+
+if (localStorage.getItem("switchState") == "checked") {
+    switchInput.checked = true;
+    background = "white";
+    border = "9F2042";
+    messages = "#646E68";
+    chats = "#9BC995";
+    words = "black";
+    actualizarColores();
+}
+
+let slider = document.createElement("span");
+slider.classList.add("slider", "round");
+
+color_switch.appendChild(switchInput);
+color_switch.appendChild(slider);
+
+div4.appendChild(color_switch);
+
+switchInput.addEventListener("change", function() {
+    if (this.checked) {
+        localStorage.setItem("switchState", "checked");
+        background = "white";
+        border = "9F2042";
+        messages = "#267891";
+        chats = "#9BC995";
+        words = "black";
+        actualizarColores();
+    } else {
+        console.log("Switch desactivado");
+        localStorage.setItem("switchState", "unchecked");
+        background = "black";
+        border = "#9F2042";
+        messages = "#9BC995";
+        chats = "#267891";
+        words = "#F8E5EE";
+        actualizarColores();
+    }
+});
+
+let textbox = document.createElement("textarea");
+textbox.className = "text";
+div5.appendChild(textbox);
+
+document.getElementsByClassName("text")[0].style.height = "50%";
+document.getElementsByClassName("text")[0].style.width = "90%";
+document.getElementsByClassName("text")[0].style.backgroundColor = '#dfe3eb';
+document.getElementsByClassName("text")[0].style.resize = "none";
+document.getElementsByClassName("text")[0].style.fontSize = "16px"
+document.getElementsByClassName("text")[0].style.fontSize = "16px"
+
+let send_button = document.createElement("button");
+send_button.id = "buttonM";
+send_button.style.borderRadius = "50%";
+send_button.style.height = "45px";
+send_button.style.width = "45px";
+send_button.style.backgroundColor = border;
+div5.appendChild(send_button);
+
+let sendI = document.createElement("img");
+sendI.id = "send";
+send_button.appendChild(sendI);
+
+document.getElementById("send").style.height = "32px";
+document.getElementById("send").style.width = "32px";
+document.getElementById("send").src = "https://static-00.iconduck.com/assets.00/send-icon-1024x1011-38wtwa0n.png";
+document.getElementById("send").style.margin = "4px 0px 0px 3px";
