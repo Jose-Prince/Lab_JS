7751bd2cb8a0975bbfe639a109d03e6d94bab979
Prince <akice@AkicePC.pc.net>
2024-02-28 17:57:04 -0600

    Feat: Image shown

--- lab.js
+++ lab.js
@@ -1,9 +1,10 @@
 document.body.style.margin = 0;
 
-let user_name = "José Prince"
+const user_name = "José Prince"
 let receptor = ""
+let mondongo = false
 
-let databurned = [[300,"José Prince", "hola", "2024"],[1000,"José Prince", "adios", "2024"], [1,"José Prince", "Buenas", "2024"], [1,"Alex", "hola", "2024"]]
+const databurned = [[300,"José Prince", "hola", "2024"],[1000,"José Prince", "adios", "2024"], [1,"José Prince", "Buenas", "2024"], [1,"Alex", "hola", "2024"]]
 
 //Paleta de colores
 let background = "black";
@@ -38,9 +39,10 @@ function actualizarColores() {
     document.getElementById("top").style.backgroundColor = chats
 
     document.getElementById("top").style.color = words
+    
 }
 
-let div1 = document.createElement("div");
+const div1 = document.createElement("div");
 div1.id = "contenedor";
 document.body.appendChild(div1);
 
@@ -50,7 +52,7 @@ document.getElementById("contenedor").style.gridTemplateRows = "94% 6%";
 document.getElementById("contenedor").style.border = "1px solid " + border
 document.getElementById("contenedor").style.height = "99.8vh";
 
-let div2 = document.createElement("div");
+const div2 = document.createElement("div");
 div2.id = "listado-chats";
 div1.appendChild(div2);
 
@@ -62,41 +64,82 @@ document.getElementById("listado-chats").style.overflow = "auto"
 
 createChats()
 
-let  div3 = document.createElement("div")
+const  div3 = document.createElement("div")
 div3.id = "mensaje"
 div1.appendChild(div3)
 
 document.getElementById("mensaje").style.border = "1px solid " + border
 document.getElementById("mensaje").style.backgroundColor = background
 
-let div3_1 = document.createElement("div")
+const div3_1 = document.createElement("div")
 div3_1.id = "top"
 div3.appendChild(div3_1)
 
-document.getElementById("top").style.minHeight = "4.7%"
-document.getElementById("top").style.maxHeight = "4.7%"
+document.getElementById("top").style.minHeight = "2.4%"
+document.getElementById("top").style.maxHeight = "2.4%"
 document.getElementById("top").style.backgroundColor = chats
 document.getElementById("top").style.border = "1px solid " + border
 document.getElementById("top").style.display = "flex"
 document.getElementById("top").style.alignItems = "center"
-document.getElementById("top").style.padding = "5px"
+document.getElementById("top").style.justifyContent = "space-between"
+document.getElementById("top").style.padding = "15px"
 document.getElementById("top").style.fontSize = "20px"
 document.getElementById("top").style.color = words
 
-let div3_2 = document.createElement("div")
+const receptorName = document.createElement("p")
+div3_1.appendChild(receptorName)
+
+const div3_1_2 = document.createElement("textarea")
+div3_1_2.id = "search"
+div3_1.appendChild(div3_1_2)
+
+document.getElementById("search").style.height = "30px";
+document.getElementById("search").style.width = "50%";
+document.getElementById("search").style.backgroundColor = '#dfe3eb';
+document.getElementById("search").style.resize = "none";
+document.getElementById("search").style.fontSize = "16px"
+document.getElementById("search").style.fontSize = "16px"
+document.getElementById("search").maxLength="140"
+document.getElementById("search").placeholder = "Buscar"
+
+document.getElementById("search").addEventListener("input", function() {
+    console.log(document.getElementById("search").value)
+
+    if (document.getElementById("search").value != ""){
+        document.getElementById("mensajes").innerHTML = ""
+
+        // const posts = await obtainPosts()
+        const posts = databurned //Cmabiar cuando el server este funcionando
+        
+        const mensajesFiltrados = posts.filter(elemento => {
+            const nombre = elemento[1]
+            const mensaje = elemento[2]
+    
+            return nombre == receptor && mensaje.toLowerCase().includes(document.getElementById("search").value.toLowerCase())
+        })
+    
+        mensajesFiltrados.forEach(element => {
+            createMessage(element[2], mondongo)
+        })
+    } else {
+        getMessages(receptor)
+    }
+})
+
+const div3_2 = document.createElement("div")
 div3_2.id = "mensajes"
 div3.appendChild(div3_2)
-
+ 
 document.getElementById("mensajes").style.padding = "15px"
 document.getElementById("mensajes").style.display = "flex"
-document.getElementById("mensajes").style.alignItems = "flex-end"
 document.getElementById("mensajes").style.justifyContent = "flex-start"
 document.getElementById("mensajes").style.flexDirection = "column-reverse"
 document.getElementById("mensajes").style.overflow = "auto"
 document.getElementById("mensajes").style.maxHeight = "90%"
 document.getElementById("mensajes").style.minHeight = "90%"
+document.getElementById("mensajes").style.width = "auto"
 
-let div4 = document.createElement("div");
+const div4 = document.createElement("div");
 div4.id = "contenido-perfil";
 div1.appendChild(div4);
 
@@ -106,9 +149,9 @@ document.getElementById("contenido-perfil").style.display = "flex"
 document.getElementById("contenido-perfil").style.alignItems = "center"
 document.getElementById("contenido-perfil").style.justifyContent = "space-around"
 
-let div5 = document.createElement("div");
-div5.id = "contenido-chat";
-div1.appendChild(div5);
+const div5 = document.createElement("div")
+div5.id = "contenido-chat"
+div1.appendChild(div5)
 
 document.getElementById("contenido-chat").style.border = "1px solid " + border;
 document.getElementById("contenido-chat").style.backgroundColor = background;
@@ -117,7 +160,7 @@ document.getElementById("contenido-chat").style.alignItems = "center";
 document.getElementById("contenido-chat").style.justifyContent = "space-evenly"
 
 //Image de perfil
-let image = document.createElement("img");
+const image = document.createElement("img");
 image.id = "perfil";
 div4.appendChild(image);
 
@@ -126,21 +169,21 @@ document.getElementById("perfil").style.width = "50px";
 document.getElementById("perfil").style.height = "50px";
 document.getElementById("perfil").style.borderRadius = "50%";
 
-let nombre_perfil = document.createElement("p");
+const nombre_perfil = document.createElement("p");
 nombre_perfil.id = "nombre";
 div4.appendChild(nombre_perfil);
 
 document.getElementById("nombre").textContent = user_name;
 document.getElementById("nombre").style.color = words;
 
-let color_switch = document.createElement("label");
+const color_switch = document.createElement("label");
 color_switch.classList.add("switch");
 
-let switchInput = document.createElement("input");
+const switchInput = document.createElement("input");
 switchInput.type = "checkbox";
 switchInput.id = "toggleSwitch";
 
-let slider = document.createElement("span");
+const slider = document.createElement("span");
 slider.classList.add("slider", "round");
 
 color_switch.appendChild(switchInput);
@@ -169,25 +212,35 @@ switchInput.addEventListener("change", function() {
     }
 });
 
-let textbox = document.createElement("textarea");
+const textbox = document.createElement("textarea");
 textbox.className = "text";
 div5.appendChild(textbox);
 
 document.getElementsByClassName("text")[0].addEventListener("keydown", function(event) {
     if (event.key === "Enter" && !event.shiftKey) {
         event.preventDefault(); // Evita que se inserte un salto de línea en el textarea
-        let contenido = document.getElementsByClassName("text")[0].value
-        if (contenido != "") {
-            createMessage(contenido);
-            // let objeto = {
-                //     "username": user_name,
-            //     "message": contenido
-            // } 
-            // postPosts(objeto)
-            document.getElementsByClassName("text")[0].value = ""; // Limpia el contenido del textarea después de enviar el mensaje
-        } 
+        const contenido = document.getElementsByClassName("text")[0].value
+                
+        const regex = /\b(?:https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|]/i
+
+        if (regex.test(contenido)){
+            mondongo = true
+            createImage(contenido, mondongo)
+        } else {
+            if (contenido != "") {
+                mondongo = true
+                createMessage(contenido, mondongo)
+                // div3_2.style.alignItems = "flex-end"
+                // const objeto = {
+                    //     "username": user_name,
+                //     "message": contenido
+                // } 
+                // postPosts(objeto)
+            } 
+        }        
+        document.getElementsByClassName("text")[0].value = ""; // Limpia el contenido del textarea después de enviar el mensaje
     }
-});
+})
 
 document.getElementsByClassName("text")[0].style.height = "50%";
 document.getElementsByClassName("text")[0].style.width = "90%";
@@ -195,10 +248,9 @@ document.getElementsByClassName("text")[0].style.backgroundColor = '#dfe3eb';
 document.getElementsByClassName("text")[0].style.resize = "none";
 document.getElementsByClassName("text")[0].style.fontSize = "16px"
 document.getElementsByClassName("text")[0].style.fontSize = "16px"
-document.getElementsByClassName("text")[0].maxLength="140"
 document.getElementsByClassName("text")[0].placeholder = "Mensaje"
 
-let send_button = document.createElement("button");
+const send_button = document.createElement("button");
 send_button.id = "theme"
 div5.appendChild(send_button);
 
@@ -208,10 +260,10 @@ document.getElementById("theme").style.width = "45px";
 document.getElementById("theme").style.backgroundColor = border;
 
 send_button.addEventListener("click", function() {
-    let contenido = document.getElementsByClassName("text")[0].value
+    const contenido = document.getElementsByClassName("text")[0].value
     if (contenido != "") {
         createMessage(contenido);
-        // let objeto = {
+        // const objeto = {
         //     "username": user_name,
         //     "message": contenido
         // } 
@@ -230,7 +282,7 @@ if (localStorage.getItem("switchState") == "checked") {
     actualizarColores();
 }
 
-let sendI = document.createElement("img");
+const sendI = document.createElement("img");
 sendI.id = "send";
 send_button.appendChild(sendI);
 
@@ -239,9 +291,8 @@ document.getElementById("send").style.width = "32px";
 document.getElementById("send").src = "https://static-00.iconduck.com/assets.00/send-icon-1024x1011-38wtwa0n.png";
 document.getElementById("send").style.margin = "4px 0px 0px 3px";
 
-function createMessage(contenido) {
-    console.log("Contenido recibido:", contenido);
-    let mensaje = document.createElement("div");
+function createMessage(contenido,mondongo) {
+    const mensaje = document.createElement("div")
     mensaje.className = "message"
     
     mensaje.style.backgroundColor = messages
@@ -252,7 +303,6 @@ function createMessage(contenido) {
     mensaje.style.alignItems = "center"
     mensaje.style.whiteSpace = "pre-line"
     mensaje.animate([
-        // {transform: "translateX(0px)"},
         {transform: "translateX(-300px)"},
         {transform: "translateX(0px)"}
     ],{
@@ -274,21 +324,58 @@ function createMessage(contenido) {
             contenido = contenido.slice(70);
         }
     }
+        phrase += contenido;
+        mensaje.innerHTML = phrase;
+    } else {
+        mensaje.innerHTML = contenido;
+    }
 
-    phrase += contenido;
-    mensaje.innerHTML = phrase;
-} else {
-    mensaje.innerHTML = contenido;
-}
-
-    div3_2.prepend(mensaje);
-
+    const div3_2_1 = document.createElement("div")
+    div3_2_1.style.display = "flex"
+    div3_2_1.style.justifyContent = (receptor == user_name || mondongo) ? "flex-end" : "flex-start"
+    div3_2.prepend(div3_2_1)
+    div3_2_1.append(mensaje);
     div3_2.scrollTop = div3_2.scrollHeight
 }
 
+function createImage(url, mondongo) {
+    const mensaje = document.createElement("div")
+    mensaje.className = "message"
+
+    mensaje.style.backgroundColor = messages
+    mensaje.style.color = "black"
+    mensaje.style.marginTop = "5px"
+    mensaje.style.padding = "5px"
+    mensaje.style.display = "flex"
+    mensaje.style.alignItems = "center"
+    mensaje.style.justifyContent = "flex-end"
+    mensaje.style.whiteSpace = "pre-line"
+    mensaje.style.width = "56%"
+    mensaje.animate([
+        {transform: "translateX(-300px)"},
+        {transform: "translateX(0px)"}
+    ],{
+        duration: 500,
+        iterations: 1,
+        fill: "forwards"
+    })
+
+    const image = document.createElement("img")
+    image.src = url
+    image.className = "imagen"
+    image.style.width = "100%"
+    mensaje.appendChild(image)
+
+    const div3_2_1 = document.createElement("div")
+    div3_2_1.style.display = "flex"
+    div3_2_1.style.justifyContent = (receptor == user_name || mondongo) ? "flex-end" : "flex-start"
+    div3_2.prepend(div3_2_1)
+    div3_2_1.append(mensaje);
+    div3_2.scrollTop = div3_2.scrollHeight 
+}
 
 function createChat(user){
-    let newChat = document.createElement("button")
+    const newChat = document.createElement("button")
     newChat.className = "chatting"
     newChat.innerText = user
     newChat.style.color = words
@@ -299,13 +386,14 @@ function createChat(user){
     newChat.style.alignItems = "center"
     newChat.style.justifyContent = "center"
     newChat.style.border = "1px solid " + border
-    newChat.style.fontSize = "17px"
+    // newChat.style.fontSize = "17px"
+
 
     newChat.addEventListener("click", function() {
         receptor = newChat.innerHTML
         
         if (receptor != document.getElementById("top").innerHTML){
-            document.getElementById("top").innerHTML = receptor
+            receptorName.textContent = receptor
             getMessages(receptor)
         }
 
@@ -317,7 +405,7 @@ function createChat(user){
 //ASYNC FUNCTIONS
 
 async function obtainPosts(){
-    let data = await fetch("http://uwu-guate.site:3000/messages",
+    const data = await fetch("http://uwu-guate.site:3000/messages",
     {
         method: "GET",
         headers: {
@@ -326,14 +414,14 @@ async function obtainPosts(){
     })
 
     console.log("await",data)
-    let posts = await data.json()
+    const posts = await data.json()
     console.log(posts)
 
     return posts
 }
 
 async function postPosts(object){
-    let data = await fetch("http://uwu-guate.site:3000/messages",
+    const data = await fetch("http://uwu-guate.site:3000/messages",
     {
         method: "POST",
         headers: {
@@ -344,14 +432,14 @@ async function postPosts(object){
 }
 
 async function createChats(){
-    let names = []
-    // let myPosts = await obtainPosts()
-    let myPosts = databurned //Cambiar cuando el server este funcionando
+    const names = []
+    // const myPosts = await obtainPosts()
+    const myPosts = databurned //Cambiar cuando el server este funcionando
 
-    let lista = document.getElementById("listado-chats")
+    const lista = document.getElementById("listado-chats")
     if (lista != null){
         myPosts.map(post=>{
-            let newChat = createChat(post[1])
+            const newChat = createChat(post[1])
             return newChat
         })
         .forEach(element => {
@@ -366,8 +454,8 @@ async function createChats(){
 async function getMessages(receptor){
     document.getElementById("mensajes").innerHTML = ""
 
-    // let posts = await obtainPosts()
-    let posts = databurned //Cmabiar cuando el server este funcionando
+    // const posts = await obtainPosts()
+    const posts = databurned //Cmabiar cuando el server este funcionando
     posts.sort((a,b) => a[0] - b[0])
 
     posts.forEach(element => {
